1935ab434fa7b6720de34be0cc5818ce23687f49
Zevez7 <datguy7@gmail.com>
2022-03-21 22:03:48 -0400

    create pthread.ppm

--- threads.c
+++ threads.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+
 int colors[64][64 * 3];
 
 //void *thread(void *vargp) {
@@ -23,22 +25,25 @@ int genRandoms() {
     return rand() % (256);
 }
 
+int counter = 0;
+
 void *paint() {
-//    printf("Artist is painting %ld \n", pthread_self());
+    printf("Artist is painting %ld \n", pthread_self());
 
     // workID corresponds to the 'artist'
     // Each artist owns one row to paint on.
     // An artist paints along each pixel 1 at a time, painting an
     // R,G,B value (that is why it is 64*3)
-//    printf("random %d", printRandoms());
+//    printf("random %d", genRandoms());
     for (int i = 0; i < 64 * 3; i++) {
-        colors[pthread_self()][i] = genRandoms(); // Try doing something more interesting with the colors!
-//        printf("%d \n", colors[pthread_self()][i]);
+        colors[counter][i] = genRandoms(); // Try doing something more interesting with the colors!
+//        printf("random %d", genRandoms());
     }
+    counter = counter + 1;
 }
 
-int savePMMFile() {
-
+int savePPMFile() {
+    printf("run savePPMfile");
     FILE *fp;
     fp = fopen("pthread.ppm", "w+");
     fputs("P3\n", fp);
@@ -61,6 +66,7 @@ int main() {
     int numberOfArtists = 64;
 
     for (int i = 0; i < numberOfArtists; i++) {
+        printf("number of Artists %d \n", i);
         pthread_create(&tid[i], NULL, paint, NULL);
     }
 
@@ -69,10 +75,10 @@ int main() {
         pthread_join(tid[i], NULL);
     }
 
-    printf("color numbers %d \n", colors[61][3]);
+    printf("color numbers %d \n", colors[62][23]);
     printf("Back to main function %ld \n", pthread_self());
 
-    savePMMFile();
+    savePPMFile();
 
 
     return 0;
